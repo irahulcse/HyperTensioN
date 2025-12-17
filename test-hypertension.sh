97102f07f307da74d10d7e77aef20b214708bf31
Rahul <1rahulchandra1@gmail.com>
2025-12-17 09:53:41 +0100

    Added the basic version of the jshop and other hypertension values

---
+++ test-hypertension.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+
+
+ruby Hype.rb examples/basic/basic.jshop examples/basic/pb1.jshop run
+
+ruby Hype.rb examples/basic/basic.jshop examples/basic/pb1.jshop rb
+
+ruby examples/basic/pb1.jshop.rb 
